62cfd5cc73b16078c50dd921e89a5e7e7e28652a
Gareth Tribello <gareth.tribello@gmail.com>
2021-02-02 15:01:35 +0000

    Added answer

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ import numpy as np
 
 def getNumberArray( N ) :
   # Your code goes here
-  return 
+  val = np.zeros(4)
+  for i in range(4) :
+     pp = 10**(3-i)
+     val[i] = np.floor( N / pp )
+     N = N - val[i]*pp
+  return val
 
 # This calls the function you have written to check if it works
 print( getNumberArray(5623) )
